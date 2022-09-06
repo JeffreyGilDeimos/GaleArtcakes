83b86d9d627696fbfd3f9bccc15c8df0fe6e186c
john-glay <johnglayc.bunao@gmail.com>
2022-09-06 13:06:23 +0800

    Navigation Done
    Added Cart button and Login button

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faCake, faComments, faUserGroup, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faCake, faComments, faUserGroup, faCartShopping, faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import { NavHashLink as NavLink } from "react-router-hash-link";
+import layered from "./img/layered.png";
 import myLogo from "./img/GaleLogo_png2.png";
 
 export default function Navigation() {
   return (
     <header id="header">
-      <div className="p-2 p-lg-3 nav-bg-color fixed-top shadow-lg">
-        <div className="container">
+      <div className="p-2 p-lg-3 nav-bg-color fixed-top">
+        <div className="container position-relative">
           <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <NavLink
               to="/"
@@ -24,7 +25,7 @@ export default function Navigation() {
                     icon={faHouse}
                     className="bi bi-house-door d-block d-flex justify-content-center mb-1 mx-auto fs-5"
                   />
-                  HOME
+                  <span className="nav-label">HOME</span>
                 </NavLink>
               </li>
               <li>
@@ -33,7 +34,7 @@ export default function Navigation() {
                     icon={faCake}
                     className="bi bi-menu-button-wide-fill d-block d-flex justify-content-center mb-1 mx-auto fs-5"
                   />
-                  MENU
+                  <span className="nav-label">CAKES</span>
                 </NavLink>
               </li>
               <li>
@@ -44,26 +45,35 @@ export default function Navigation() {
                 >
                   <FontAwesomeIcon
                     icon={faComments} className="bi bi-megaphone d-block d-flex justify-content-center mb-1  mx-auto fs-5" />
-                  REVIEWS
+                  <span className="nav-label">REVIEWS</span>
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/About" className="nav-link">
                 <FontAwesomeIcon
                     icon={faUserGroup} className="bi bi-people d-block d-flex justify-content-center mb-1 mx-auto fs-5" />
-                  ABOUT
+                  <span className="nav-label">ABOUT</span>
                 </NavLink>
               </li>
               <li>
-                <NavLink to="/Login" className="nav-link">
-                <FontAwesomeIcon
+                <NavLink to="/cart" className="nav-link position-relative">
+                  <FontAwesomeIcon
                     icon={faCartShopping} className="bi bi-people d-block d-flex justify-content-center mb-1 mx-auto fs-5" />
-                  CART
+                    <span className="position-absolute cart-number translate-middle badge rounded-pill b-primary">10</span>
+                    <span className="nav-label">CART</span>
+                </NavLink>
+              </li>
+              <li className="login">
+                <NavLink to="/login" className="nav-link text-white d-flex align-items-center btn-login  rounded-pill">
+                <FontAwesomeIcon
+                    icon={faRightToBracket} className="bi bi-people mx-auto fs-5 me-2" />
+                  LOGIN
                 </NavLink>
               </li>
             </ul>
           </div>
         </div>
+        <img src={layered} alt="layered" className="layered img-fluid w-100 position-absolute m-0" />
       </div>
     </header>
   );
