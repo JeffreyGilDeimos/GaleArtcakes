b866ee08843deb8ea1f0b30fc6a448ce273aae87
Jeffrey Gil P. Deimos <jeffroxgil@gmail.com>
2022-09-06 15:14:11 +0800

    Update Logo and feedback9.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faCake, faComments, faUserGroup, faCartShopping, faRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHouse,
+  faCake,
+  faComments,
+  faUserGroup,
+  faCartShopping,
+  faRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
 import { NavHashLink as NavLink } from "react-router-hash-link";
 
 export default function Navigation() {
@@ -14,7 +21,12 @@ export default function Navigation() {
               className="d-flex align-items-center my-1
               my-lg-0 me-lg-auto text-decoration-none"
             >
-              <img src="images/GaleLogo_png2.png" width="180" height="75" alt="Gale Logo" />
+              <img
+                src="images/Logo.png"
+                width="180"
+                height="75"
+                alt="Gale Logo"
+              />
             </NavLink>
             <ul className="nav col-12 col-lg-auto my-2 justify-content-center my-lg-1 text-small fw-bold">
               <li>
@@ -42,36 +54,53 @@ export default function Navigation() {
                   className="nav-link"
                 >
                   <FontAwesomeIcon
-                    icon={faComments} className="bi bi-megaphone d-block d-flex justify-content-center mb-1  mx-auto fs-5" />
+                    icon={faComments}
+                    className="bi bi-megaphone d-block d-flex justify-content-center mb-1  mx-auto fs-5"
+                  />
                   <span className="nav-label">REVIEWS</span>
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/About" className="nav-link">
-                <FontAwesomeIcon
-                    icon={faUserGroup} className="bi bi-people d-block d-flex justify-content-center mb-1 mx-auto fs-5" />
+                  <FontAwesomeIcon
+                    icon={faUserGroup}
+                    className="bi bi-people d-block d-flex justify-content-center mb-1 mx-auto fs-5"
+                  />
                   <span className="nav-label">ABOUT</span>
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/cart" className="nav-link position-relative">
                   <FontAwesomeIcon
-                    icon={faCartShopping} className="bi bi-people d-block d-flex justify-content-center mb-1 mx-auto fs-5" />
-                    <span className="position-absolute cart-number translate-middle badge rounded-pill b-primary">10</span>
-                    <span className="nav-label">CART</span>
+                    icon={faCartShopping}
+                    className="bi bi-people d-block d-flex justify-content-center mb-1 mx-auto fs-5"
+                  />
+                  <span className="position-absolute cart-number translate-middle badge rounded-pill b-primary">
+                    10
+                  </span>
+                  <span className="nav-label">CART</span>
                 </NavLink>
               </li>
               <li className="login">
-                <NavLink to="/login" className="nav-link text-white d-flex align-items-center btn-login  rounded-pill">
-                <FontAwesomeIcon
-                    icon={faRightToBracket} className="bi bi-people mx-auto fs-5 me-2" />
+                <NavLink
+                  to="/login"
+                  className="nav-link text-white d-flex align-items-center btn-login  rounded-pill"
+                >
+                  <FontAwesomeIcon
+                    icon={faRightToBracket}
+                    className="bi bi-people mx-auto fs-5 me-2"
+                  />
                   LOGIN
                 </NavLink>
               </li>
             </ul>
           </div>
         </div>
-        <img src="images/layered.png" alt="layered" className="layered img-fluid w-100 position-absolute m-0" />
+        <img
+          src="images/layered.png"
+          alt="layered"
+          className="layered img-fluid w-100 position-absolute m-0"
+        />
       </div>
     </header>
   );
