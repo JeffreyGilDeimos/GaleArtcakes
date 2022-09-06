4bd5140f6c6aba07442f99160bc90ec23f1aa9dd
Jeffrey Gil P. Deimos <jeffroxgil@gmail.com>
2022-09-06 17:10:10 +0800

    Created new collection and button link..

--- src/components/NewCollections.jsx
+++ src/components/NewCollections.jsx
@@ -1,19 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { cakeList } from "../utilities/enums";
 
 export default function NewCollections() {
   const renderCakeList = () => {
     return cakeList.map((item) => (
-      <div
-        class="col-sm-6 col-md-4 col-lg-3 pb-4 d-flex justify-content-center"
-        key={item.id}
-      >
-        <div class="card-img overflow-hidden">
-          <img
-            src={item.image}
-            alt={item.name}
-            class="img-fluid rounded shadow-lg"
-          />
+      <div className="col-sm-6 col-md-4 col-lg-3 pb-4" key={item.id}>
+        <div className="card-img-new overflow-hidden rounded position-relative">
+          <img src={item.image} alt={item.name} className="img-fluid " />
+          <span className="position-absolute d-flex align-items-center justify-content-center text-danger fs-3">
+            <FontAwesomeIcon icon={faHeart} />
+          </span>
+        </div>
+        <div className="text-center">
+          <p className="text-capitalize mt-3 mb-1 fs-5">{item.name}</p>
+          <span className="fw-bold d-block">{item.price}</span>
+
+          <Link to={`/Cake/${item.id}`} className="btn btn-danger mt-3">
+            Add to Cart
+          </Link>
         </div>
       </div>
     ));
@@ -21,14 +28,12 @@ export default function NewCollections() {
 
   return (
     <div>
-      <section id="varities" class="bg-danger bg-opacity-10">
-        <div class="text-center py-5">
-          <h4>
-            <i class="bi bi-check-square me-2"></i>NEW COLLECTIONS
-          </h4>
+      <section id="varities">
+        <div className="text-center py-5">
+          <h4>NEW COLLECTIONS</h4>
         </div>
-        <div class="container pb-5">
-          <div class="row">{renderCakeList()}</div>
+        <div className="container pb-5">
+          <div className="row">{renderCakeList()}</div>
         </div>
       </section>
     </div>
