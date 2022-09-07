7774106b9ffafebbb1e88934ad5718b0465af00e
Jeffrey Gil P. Deimos <jeffroxgil@gmail.com>
2022-09-07 17:11:30 +0800

    Filtered NewCollection from enums..

--- src/components/NewCollections.jsx
+++ src/components/NewCollections.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { cakeList } from "../utilities/enums";
 
 export default function NewCollections() {
+  const [activeFilter] = useState("YES");
+  const [cakes, setCakes] = useState(cakeList);
+
+  useEffect(() => {
+    setCakes(cakeList.filter((item) => item.featured === activeFilter));
+  }, [activeFilter]);
+
   const renderCakeList = () => {
-    return cakeList.map((item) => (
+    return cakes.map((item) => (
       <div className="col-sm-6 col-md-4 col-lg-3 pb-4" key={item.id}>
         <div className="card-img-new overflow-hidden rounded position-relative">
           <img src={item.image} alt={item.name} className="img-fluid " />
@@ -19,7 +26,7 @@ export default function NewCollections() {
           <span className="fw-bold d-block">{item.price}</span>
 
           <Link to={`/Cake/${item.id}`} className="btn btn-danger mt-3">
-            Add to Cart
+            More details
           </Link>
         </div>
       </div>
