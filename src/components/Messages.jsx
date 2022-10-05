8fd808aae125701b67f4b80f5fe0258fa0270a77
john-glay <johnglayc.bunao@gmail.com>
2022-10-05 17:48:14 +0800

    Edit, Delete, Save UI Update

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -2,21 +2,29 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 import TextareaAutosize from "react-textarea-autosize";
+import { useSelector } from "react-redux";
 
-export default function Messages({ user, userImage, message, timestamp }) {
+export default function Messages({
+  user,
+  userImage,
+  message,
+  timestamp,
+  email,
+}) {
   const [action, setAction] = useState("");
+  const activeUser = useSelector((state) => state.activeUser);
 
   return (
     <div className="p-0 m-0">
       <div className="review-card bg-white p-3 rounded-3 h-100 position-relative">
-        <div className="d-flex justify-content-between mb-3">
+        <div className="d-flex mb-3">
           <div className="review-head d-flex align-items-center">
             <img
               src={userImage}
               alt="profile-review"
               className="review-profile rounded-circle"
             />
-            <figure className="ms-3 mb-0">
+            <figure className="ms-3 me-3 mb-0">
               <blockquote className="blockquote m-0 fs-6">
                 <p>
                   <strong>{user}</strong>
@@ -27,28 +35,42 @@ export default function Messages({ user, userImage, message, timestamp }) {
               </figcaption>
             </figure>
           </div>
-          <div className="position-relative review-icon ps-3" onClick={() => setAction(action ? "" : "d")}>
-            <FontAwesomeIcon icon={faEllipsisVertical} />
-            <div className={`d-none${action} review-actions position-absolute bg-white p-2 border border-2 rounded`}>
-              <button className="edit-review border-0 p-0 bg-white fs-6 px-2">
-                <small>Edit</small>
-              </button>
-              <hr className="mx-0 mt-2 mb-1" />
-              <button className="delete-review border-0 p-0 bg-white fs-6 px-2">
-                <small>Delete</small>
+        </div>
+
+        {activeUser.email === email && activeUser.email ? (
+          <>
+            <div className="review-icon position-absolute p-3">
+              <div
+                className="position-relative"
+                onClick={() => setAction(action ? "" : "d")}
+              >
+                <FontAwesomeIcon icon={faEllipsisVertical} />
+                <div
+                  className={`d-none${action} review-actions position-absolute bg-white p-2 border border-2 rounded`}
+                >
+                  <button className="edit-review border-0 p-0 bg-white fs-6 px-2">
+                    <small>Edit</small>
+                  </button>
+                  <hr className="mx-0 mt-2 mb-1" />
+                  <button className="delete-review border-0 p-0 bg-white fs-6 px-2">
+                    <small>Delete</small>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="for-save-btn d-flex justify-content-end position-absolute pb-3 pe-3">
+              <button className="save-review border-0 p-0 bg-white fs-6 fw-bolder">
+                <small>Save</small>
               </button>
             </div>
-          </div>
-        </div>
-        <div className="for-save-btn d-flex justify-content-end position-absolute pb-3 pe-3">
-          <button className="save-review border-0 p-0 bg-white fs-6 fw-bolder">
-            <small>Save</small>
-          </button>
-        </div>
+          </>
+        ) : (
+          <div></div>
+        )}
         <div className="review-body">
           <TextareaAutosize
             defaultValue={message}
-            className="m-0 review w-100 border-0 p-2"
+            className="m-0 review w-100 h-100 border-0 p-2"
             readOnly
           />
         </div>
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -6,6 +6,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Messages from "./Messages";
+import TextareaAutosize from "react-textarea-autosize";
 
 export default function Reviews() {
   const [input, setInput] = useState("");
@@ -19,12 +20,25 @@ export default function Reviews() {
   const sendMessage = (e) => {
     e.preventDefault();
 
-    db.collection("messages").add({
+    if (activeUser.id) {
+      db.collection("messages").add({
       message: input,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      user: user.displayName,
-      userImage: user.photoURL,
+      user: activeUser.email,
+      userImage: "https://th.bing.com/th/id/R.d268b238932809e18b85a7820184220f?rik=ahExR0U%2fu2zHyQ&riu=http%3a%2f%2ficon-library.com%2fimages%2fno-profile-picture-icon%2fno-profile-picture-icon-2.jpg&ehk=4X8pLfMkepeJcdTMZ8L033nQ2hfH0gJN3qGTpg62g00%3d&risl=&pid=ImgRaw&r=0",
+      email: activeUser.email,
     });
+    } else {
+      db.collection("messages").add({
+        message: input,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        user: user.displayName,
+        userImage: user.photoURL,
+        email: activeUser.email,
+      });
+    }
+
+    
 
     setInput("");
   };
@@ -47,7 +61,7 @@ export default function Reviews() {
               onSubmit={sendMessage}
               className="d-md-flex justify-content-center align-items-center"
             >
-              <textarea
+              <TextareaAutosize
                 name="review-input"
                 id="review-input"
                 value={input}
@@ -55,7 +69,7 @@ export default function Reviews() {
                 onClick={() => checkUser()}
                 placeholder="Write your review here!"
                 className="rounded-3"
-              ></textarea>
+              />
               <button className="submit-review text-decoration-none rounded-3 text-white d-block mx-auto m-md-0 mt-2 mt-md-0 ms-md-4 text-uppercase fw-bold">
                 Submit
               </button>
@@ -67,7 +81,7 @@ export default function Reviews() {
                 {/* REVIEWS */}
 
                 {reviews?.docs.map((doc) => {
-                  const { user, userImage, message, timestamp } = doc.data();
+                  const { user, userImage, message, timestamp, email } = doc.data();
                   return (
                     <Messages
                       key={doc.id}
@@ -75,6 +89,7 @@ export default function Reviews() {
                       userImage={userImage}
                       message={message}
                       timestamp={timestamp}
+                      email={email}
                     />
                   );
                 })}
